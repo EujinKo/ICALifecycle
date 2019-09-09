7b1f0b38fe2996aabeaab95c6937719c3959250c
EujinKo <42593556+EujinKo@users.noreply.github.com>
2019-09-09 02:19:29 -0700

    working

--- app/src/main/java/com/example/icalifecycle/MainActivity.java
+++ app/src/main/java/com/example/icalifecycle/MainActivity.java
@@ -8,16 +8,25 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static String str ="";
+    private static int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
-
+    @Override
     protected void onResume(){
         super.onResume();
-        System.out.println("On start");
+        //System.out.println("On start"+count);
+        str += "resume "+count+"\n";
+        Intent intent = getIntent();
+        intent.putExtra(EXTRA_MESSAGE,str);
+
+        count++;
     }
 
     public void goToOther(View view){
