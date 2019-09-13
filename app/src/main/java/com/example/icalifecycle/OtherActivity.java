6d43b97a3291e765d0ce0871766d30d1333eb5fb
EujinKo <42593556+EujinKo@users.noreply.github.com>
2019-09-12 17:16:40 -0700

    ????

--- app/src/main/java/com/example/icalifecycle/OtherActivity.java
+++ app/src/main/java/com/example/icalifecycle/OtherActivity.java
@@ -14,9 +14,9 @@ import java.util.List;
 
 public class OtherActivity extends AppCompatActivity {
 
-    //static List<String> shoppingList = new ArrayList<String>(Arrays.asList(
-     //       "one","two","three","Four","five","six","seven","eight","nine","ten"
-    //));
+//    static List<String> shoppingList = new ArrayList<String>(Arrays.asList(
+//            "one","two","three","Four","five","six","seven","eight","nine","ten"
+//    ));
 
 
     static String[] shoppingList = new String[]{"strawberry","banana","cat","grape"};
@@ -34,34 +34,48 @@ public class OtherActivity extends AppCompatActivity {
         ListView shoppingListView = (ListView)findViewById(R.id.shopping_list_view);
 
 
-        /*
-        //Set up ArrrayAdpater filled with strings in ArrayList
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                R.layout.shopping_list_row,R.id.shopping_list_row_item, shoppingList);
 
-        shoppingListView.setAdapter(arrayAdapter);
-         */
+//        //Set up ArrrayAdpater filled with strings in ArrayList
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+//                R.layout.shopping_list_row,R.id.shopping_list_row_item, shoppingList);
+//
+//        shoppingListView.setAdapter(arrayAdapter);
+
 
         //Set up SimpleAdapter filled with image, strings
+        List<HashMap<String, String>> aList =
+                new ArrayList<HashMap<String, String>>();
 
-        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
-
-        for(int i=0;i<shoppingListImages.length;i++){
-            HashMap<String,String> hm = new HashMap<String,String>();
-            hm.put("shopping_list_row_image",Integer.toString(shoppingListImages[i]));
-            hm.put("shopping_list_row_text",shoppingList[i]);
+        for (int i = 0; i < shoppingListImages.length; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("shopping_list_row_image", Integer.toString(shoppingListImages[i])+"");
+            hm.put("shopping_list_row_text", shoppingList[i]);
             aList.add(hm);
         }
 
+        String[] from = {"shopping_list_row_image", "shopping_list_row_text"};
+        int[] to = {R.id.shopping_list_row_image, R.id.shopping_list_row_text};
 
-        String[] from = {"shopping_list","shopping_ist_row_text"};
-        int[] to = {R.id.shopping_list_row_image,R.id.shopping_list_row_text};
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(),
-                aList,R.layout.shopping_list_row,from,to);
+        SimpleAdapter simpleAdapter =
+                new SimpleAdapter(getBaseContext(), aList,
+                        R.layout.shopping_list_row, from, to);
 
         shoppingListView = (ListView) findViewById(R.id.shopping_list_view);
         shoppingListView.setAdapter(simpleAdapter);
+
+        // Get a reference to the ListView
+        shoppingListView =
+                (ListView)findViewById(R.id.shopping_list_view);
+
+        // Create a new Array Adapter
+        // Specify which layout and view to use for a row
+        // and the data (array) to use
+        ArrayAdapter<String> arrayAdapter = new
+                ArrayAdapter<String>(this, R.layout.shopping_list_row,
+                R.id.shopping_list_row_text, shoppingList);
+
+        // Link the ListView and the Adapter
+        shoppingListView.setAdapter(arrayAdapter);
 
 
     }
